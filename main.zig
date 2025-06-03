6386febafd8b464ef60d2f954a171725f91c2779
vlad <vlad@nikulin.name>
2025-06-03 13:58:47 +0100

    implement println, change printed out message

--- main.zig
+++ main.zig
@@ -8,9 +8,18 @@ fn print_uart(msg: []const u8) void {
     }
 }
 
+fn println(comptime msg: []const u8) void { // msg must be fully known at comp time
+    const new_msg = msg ++ "\n";
+    print_uart(new_msg);
+}
+
 pub export fn c_entry() callconv(.C) void {
-    const msg = "Hello Zig World!\n";
-    print_uart(msg);
+    println("this is a hello world example");
+    println("this is running on a bare metal machine");
+    println("there is no operating system");
+    println("no windows, no linux, no MacOS");
+    println("only my own code");
+    println("perfectly balanced, as all things should be");
 
     while (true) {} // halt
 }
