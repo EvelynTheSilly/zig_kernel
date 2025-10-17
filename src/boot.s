ca03eed062069355795182b68b8c1b8b404c7b64
Evelyn <45495711+EvelynTheSilly@users.noreply.github.com>
2025-10-17 19:51:54 +0100

    added reg dumping and saving

--- src/boot.s
+++ src/boot.s
@@ -16,34 +16,178 @@ _vector_table:
 
 // ========== Exceptions from EL0 ==========
 el0_sync:
+    // --- Dump registers --- 
+    stp     x19, x20, [sp, #-16]!
+    stp     x21, x22, [sp, #-16]!
+    stp     x23, x24, [sp, #-16]!
+    stp     x25, x26, [sp, #-16]!
+    stp     x27, x28, [sp, #-16]!
+    stp     x29, x30, [sp, #-16]!
+
     b el0_sync_handler
+
+    // --- Restore registers ---
+    ldp     x29, x30, [sp], #16
+    ldp     x27, x28, [sp], #16
+    ldp     x25, x26, [sp], #16
+    ldp     x23, x24, [sp], #16
+    ldp     x21, x22, [sp], #16
+    ldp     x19, x20, [sp], #16
+    
+    eret    
     .space 128 - (. - el0_sync)
 
 el0_irq:
+    // --- Dump registers --- 
+    stp     x19, x20, [sp, #-16]!
+    stp     x21, x22, [sp, #-16]!
+    stp     x23, x24, [sp, #-16]!
+    stp     x25, x26, [sp, #-16]!
+    stp     x27, x28, [sp, #-16]!
+    stp     x29, x30, [sp, #-16]!
+
     b el0_irq_handler
+
+    // --- Restore registers ---
+    ldp     x29, x30, [sp], #16
+    ldp     x27, x28, [sp], #16
+    ldp     x25, x26, [sp], #16
+    ldp     x23, x24, [sp], #16
+    ldp     x21, x22, [sp], #16
+    ldp     x19, x20, [sp], #16
+    
+    eret   
     .space 128 - (. - el0_irq)
 
 el0_fiq:
+    // --- Dump registers --- 
+    stp     x19, x20, [sp, #-16]!
+    stp     x21, x22, [sp, #-16]!
+    stp     x23, x24, [sp, #-16]!
+    stp     x25, x26, [sp, #-16]!
+    stp     x27, x28, [sp, #-16]!
+    stp     x29, x30, [sp, #-16]!
+
     b el0_fiq_handler
+
+    // --- Restore registers ---
+    ldp     x29, x30, [sp], #16
+    ldp     x27, x28, [sp], #16
+    ldp     x25, x26, [sp], #16
+    ldp     x23, x24, [sp], #16
+    ldp     x21, x22, [sp], #16
+    ldp     x19, x20, [sp], #16
+    
+    eret   
     .space 128 - (. - el0_fiq)
 
 el0_serror:
+    // --- Dump registers --- 
+    stp     x19, x20, [sp, #-16]!
+    stp     x21, x22, [sp, #-16]!
+    stp     x23, x24, [sp, #-16]!
+    stp     x25, x26, [sp, #-16]!
+    stp     x27, x28, [sp, #-16]!
+    stp     x29, x30, [sp, #-16]!
+
     b el0_serror_handler
+
+    // --- Restore registers ---
+    ldp     x29, x30, [sp], #16
+    ldp     x27, x28, [sp], #16
+    ldp     x25, x26, [sp], #16
+    ldp     x23, x24, [sp], #16
+    ldp     x21, x22, [sp], #16
+    ldp     x19, x20, [sp], #16
+    
+    eret   
     .space 128 - (. - el0_serror)
 
 // ========== Exceptions from EL1 ==========
 el1_sync:
+    // --- Dump registers --- 
+    stp     x19, x20, [sp, #-16]!
+    stp     x21, x22, [sp, #-16]!
+    stp     x23, x24, [sp, #-16]!
+    stp     x25, x26, [sp, #-16]!
+    stp     x27, x28, [sp, #-16]!
+    stp     x29, x30, [sp, #-16]!
+
     b el1_sync_handler
+
+    // --- Restore registers ---
+    ldp     x29, x30, [sp], #16
+    ldp     x27, x28, [sp], #16
+    ldp     x25, x26, [sp], #16
+    ldp     x23, x24, [sp], #16
+    ldp     x21, x22, [sp], #16
+    ldp     x19, x20, [sp], #16
+    
+    eret   
     .space 128 - (. - el1_sync)
 
 el1_irq:
+    // --- Dump registers --- 
+    stp     x19, x20, [sp, #-16]!
+    stp     x21, x22, [sp, #-16]!
+    stp     x23, x24, [sp, #-16]!
+    stp     x25, x26, [sp, #-16]!
+    stp     x27, x28, [sp, #-16]!
+    stp     x29, x30, [sp, #-16]!
+
     b el1_irq_handler
+
+    // --- Restore registers ---
+    ldp     x29, x30, [sp], #16
+    ldp     x27, x28, [sp], #16
+    ldp     x25, x26, [sp], #16
+    ldp     x23, x24, [sp], #16
+    ldp     x21, x22, [sp], #16
+    ldp     x19, x20, [sp], #16
+    
+    eret   
     .space 128 - (. - el1_irq)
 
 el1_fiq:
+    // --- Dump registers --- 
+    stp     x19, x20, [sp, #-16]!
+    stp     x21, x22, [sp, #-16]!
+    stp     x23, x24, [sp, #-16]!
+    stp     x25, x26, [sp, #-16]!
+    stp     x27, x28, [sp, #-16]!
+    stp     x29, x30, [sp, #-16]!
+
     b el1_fiq_handler
+
+    // --- Restore registers ---
+    ldp     x29, x30, [sp], #16
+    ldp     x27, x28, [sp], #16
+    ldp     x25, x26, [sp], #16
+    ldp     x23, x24, [sp], #16
+    ldp     x21, x22, [sp], #16
+    ldp     x19, x20, [sp], #16
+    
+    eret   
     .space 128 - (. - el1_fiq)
 
 el1_serror:
+    // --- Dump registers --- 
+    stp     x19, x20, [sp, #-16]!
+    stp     x21, x22, [sp, #-16]!
+    stp     x23, x24, [sp, #-16]!
+    stp     x25, x26, [sp, #-16]!
+    stp     x27, x28, [sp, #-16]!
+    stp     x29, x30, [sp, #-16]!
+
     b el1_serror_handler
+
+    // --- Restore registers ---
+    ldp     x29, x30, [sp], #16
+    ldp     x27, x28, [sp], #16
+    ldp     x25, x26, [sp], #16
+    ldp     x23, x24, [sp], #16
+    ldp     x21, x22, [sp], #16
+    ldp     x19, x20, [sp], #16
+    
+    eret   
     .space 128 - (. - el1_serror)
