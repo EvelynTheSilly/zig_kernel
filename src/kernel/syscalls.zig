12febb6d7f8df5ac4967d1ffb3c35c3c1c66985c
Evelyn <45495711+EvelynTheSilly@users.noreply.github.com.>
2025-12-27 01:31:22 +0000

    implement call convention stuff for syscall things :3

--- src/kernel/syscalls.zig
+++ src/kernel/syscalls.zig
@@ -37,7 +37,25 @@ pub export fn el1_spx_serror_handler() align(16) callconv(.c) void {
 
 // lower el interupts
 // aarch64
-pub export fn el0_aarch64_sync_handler() align(16) callconv(.c) i64 {
+// MAIN SYSCALL HANDLER
+pub export fn el0_aarch64_sync_handler(
+    arg0: i64, // syscall ID
+    arg1: i64, // arg 1
+    arg2: i64, // arg 2
+    arg3: i64, // arg 3
+    arg4: i64, // arg 4
+    arg5: i64, // arg 5
+    arg6: i64, // arg 6
+    arg7: i64, // arg 7
+) align(16) callconv(.c) i64 {
+    _ = arg0; // autofix
+    _ = arg1; // autofix
+    _ = arg2; // autofix
+    _ = arg3; // autofix
+    _ = arg4; // autofix
+    _ = arg5; // autofix
+    _ = arg6; // autofix
+    _ = arg7; // autofix
     println("hello, this is an interupt, it will now return");
     return 42;
 }
