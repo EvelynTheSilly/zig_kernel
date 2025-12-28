f0b5c0aa370aa37452d29c8dc595d5c273adecb0
Evelyn <45495711+EvelynTheSilly@users.noreply.github.com.>
2025-12-28 20:54:52 +0000

    finish zig hello world

--- src/userspace/zig_hello_world/main.zig
+++ src/userspace/zig_hello_world/main.zig
@@ -1,7 +1,7 @@
 export fn _INIT() void {
     // call the write syscall
     // explicitly discard any possible errors because this call doesnt return errors
-    _ = write("hello world!");
+    _ = write("hello world!\n");
     // hang forever
     while (true) {
         asm volatile ("wfi");
@@ -16,9 +16,9 @@ pub fn write(buf: []const u8) usize {
     //\\ mov x1, {ptr}
     //\\ mov x2, {len}
         \\ svc #0
-        : [ret] "=x0" (-> usize),
-        : [id] "x0" (id),
-          [ptr] "x1" (buf.ptr),
-          [len] "x2" (buf.len),
+        : [ret] "=&{x0}" (-> usize),
+        : [id] "{x0}" (id),
+          [ptr] "{x1}" (buf.ptr),
+          [len] "{x2}" (buf.len),
         : .{ .x0 = true, .x1 = true, .x2 = true, .x3 = true, .x4 = true, .x5 = true, .x6 = true, .x7 = true, .x8 = true, .x9 = true, .x10 = true, .x11 = true, .x12 = true, .x13 = true, .x14 = true, .x15 = true, .x16 = true, .x17 = true, .x18 = true });
 }
