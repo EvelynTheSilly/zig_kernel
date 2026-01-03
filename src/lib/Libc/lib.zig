bdd5e0d09a2faff04b6ba05f101076796cabbdd4
Evelyn <45495711+EvelynTheSilly@users.noreply.github.com.>
2026-01-03 15:00:22 +0000

    DOC: document the export symbols function

--- src/lib/Libc/lib.zig
+++ src/lib/Libc/lib.zig
@@ -2,6 +2,14 @@ const memset = @import("mem/memset.zig");
 const memcpy = @import("mem/memcpy.zig");
 const memmove = @import("mem/memmove.zig");
 
+/// exports the symbols from the library
+/// does nothing outside of a comptime context
+///
+/// # usage
+/// ```zig
+/// const libc = @import("libc");
+/// comptime libc.export_symbols();
+/// ```
 pub fn export_symbols() void {
     _ = memset;
     _ = memcpy;
