aba102c4440a1d3a315c229cabd6ab4424f34d41
Evelyn <45495711+EvelynTheSilly@users.noreply.github.com.>
2026-01-10 22:59:11 +0000

    fix build.sh in accorance of the previous arch change

--- src/kernel/build.sh
+++ src/kernel/build.sh
@@ -2,14 +2,12 @@ set -e
 
 ZIG=zig
 CROSS=aarch64-none-elf
-ARCH=aarch64
-
 
 $ZIG build
 
 # Assemble startup
-$CROSS-as -c ./arch/$ARCH/boot.s -o $BYPRODUCTS/boot.o
-$CROSS-as -c ./arch/$ARCH/interupts.s -o $BYPRODUCTS/interupts.o
+$CROSS-as -c ./asm/boot.s -o $BYPRODUCTS/boot.o
+$CROSS-as -c ./asm/interupts.s -o $BYPRODUCTS/interupts.o
 
 # Link kernel
 $CROSS-ld -r $BYPRODUCTS/boot.o $BYPRODUCTS/kernel.o $BYPRODUCTS/interupts.o -o $OUT/kernel.o
